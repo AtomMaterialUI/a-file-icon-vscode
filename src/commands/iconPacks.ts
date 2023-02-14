92ec6c35b0c5544d926fa324db2150198869fc5a
Elior <heliosaian@gmail.com>
2023-02-14 14:56:18 +0200

    refactor(locales): remvoe dynamic loading

--- src/commands/iconPacks.ts
+++ src/commands/iconPacks.ts
@@ -1,7 +1,9 @@
 import { iconPackManager } from 'src/helpers/IconPackManager';
+import { logger } from 'src/helpers/LoggingService';
 
 /** Command to toggle the icons packs */
 export const toggleIconPacks = async () => {
+  logger.info('Open toggle icon pack popup');
   await iconPackManager.toggleIconPacks();
 };
 
--- src/i18n/i18next.ts
+++ src/i18n/i18next.ts
@@ -1,5 +1,4 @@
 import i18next from 'i18next';
-import resourcesToBackend from 'i18next-resources-to-backend';
 import type { EnvLanguage } from 'src/@types/languages';
 import { env } from 'vscode';
 
@@ -13,13 +12,6 @@ const getCurrentLanguage = () => env.language as EnvLanguage;
  */
 export const initI18next = async () => {
   await i18next
-    .use(
-      resourcesToBackend((language: string, _, callback) =>
-        import(`src/locales/${language}.json`)
-          .then((resources) => callback(null, resources))
-          .catch((error) => callback(error, null)),
-      ),
-    )
     .init({
       lng: getCurrentLanguage(),
       fallbackLng: 'en',
@@ -30,10 +22,21 @@ export const initI18next = async () => {
       interpolation: {
         escapeValue: false,
       },
+      resources: {
+        // Cannot find a way to use dynamic import with module: commonjs, so fuck it and lets load them all
+        en: { translation: await import('./locales/en.json') },
+        de: { translation: await import('./locales/de.json') },
+        es: { translation: await import('./locales/es.json') },
+        fr: { translation: await import('./locales/fr.json') },
+        nl: { translation: await import('./locales/nl.json') },
+        ja: { translation: await import('./locales/ja.json') },
+        pl: { translation: await import('./locales/pl.json') },
+        'pt-br': { translation: await import('./locales/pt-br.json') },
+        'pt-pt': { translation: await import('./locales/pt-pt.json') },
+        ru: { translation: await import('./locales/ru.json') },
+        uk: { translation: await import('./locales/uk.json') },
+        'zh-cn': { translation: await import('./locales/zh-cn.json') },
+        'zh-tw': { translation: await import('./locales/zh-tw.json') },
+      },
     });
 };
-
-
-
-
-
